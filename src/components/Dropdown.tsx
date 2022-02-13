81c401f6e31664e318dc5dc4f383f72289c9402d
Michal <juchniewicz.m@gmail.com>
2022-02-13 20:57:53 +0100

    Added filter functionality to Dropdown componenet

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,18 +1,21 @@
 import './Dropdown.scss'
 import {ITableData} from '../interfaces/Interfaces'
 import { useState, useRef, useEffect, SyntheticEvent } from 'react'
+import { getByDisplayValue } from '@testing-library/react'
 
 interface DropdownProps {
     options: ITableData[],
     prompt: string,
     value: string | null,
-    onChange: (a: string) => void,
+    onChange: (a: string | null) => void,
     id: string,
-    label: string
+    label: any
 }
 
 const Dropdown = ({options, prompt, value, onChange, id, label}: DropdownProps ) => {
     const [open, setOpen] = useState(false)
+    const [query, setQuery] = useState("")
+
     const ref = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
@@ -23,25 +26,45 @@ const Dropdown = ({options, prompt, value, onChange, id, label}: DropdownProps )
     function close(e: MouseEvent) {
         setOpen(e && e.target === ref.current)
     }
+
+    function filter(options: ITableData[]) {
+        return options.filter((option: any) => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1)
+    }
+
+    function displayValue() {
+        if(query.length > 0) return query;
+        if(value) return value[label];
+        return ""
+    }
     return ( 
         <div className="dropdown">
             <div className="control" onClick={() => setOpen(prev => !prev) }>
-                <div className="selected-value" ref={ref}>{
-                    value ? label : prompt
-                }</div>
+                <div className="selected-value" ref={ref}>
+                    <input type="text" 
+                    
+                    placeholder={value ? value : prompt}
+                    value={displayValue()}
+                    onChange={e => {
+                        setQuery(e.target.value)
+                        onChange(null)
+                    }}
+                    onClick={() => setOpen(prev => !prev)}
+                    />
+                </div>
                 <div className={`arrow ${open ? "open" : null}`} />
             </div>
             <div className={`options ${open ? "open" : null}`} >
                 {
-                    options.map((option: ITableData) => 
+                    filter(options).map((option: any) => 
                     <div 
-                        key={option.id}
-                        className={`option ${value === option.assignee ? "selected" : null}`}
+                        key={option[id]}
+                        className={`option ${value === option[label] ? "selected" : null}`}
                             onClick={() => {
-                            onChange(option.assignee);
-                            setOpen(false);
+                                setQuery("")
+                                onChange(option[label]);
+                                setOpen(false);
                     }}>
-                        {option.label}
+                        {option[label]}
                     </div>)
                 }
                 
